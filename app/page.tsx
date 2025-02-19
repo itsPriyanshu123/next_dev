7575503a6e10574874e846becdec08afd6aa55bb
itsPriyanshu123 <priyanshujee2017@gmail.com>
2025-02-19 16:41:50 +0530

    tailwind setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <h1 className="text-3xl text-violet-500">Welocome in the world of next js </h1>
+  <h1 className="text-3xl text-primary-light">
+    Welocome  sdsdsdsd sdsdsdsd sdsdsdsdsdsdsdsds sdsdsd in the 
+    ddfdf dffsfsfsfhish sfidsih world of next js
+     </h1>
   );
 }
