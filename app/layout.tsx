f87c61dde118655c570d9a2cb4962c350a0174cd
Priyanshu Dube <priyanshu.dube@suryodaybank.com>
2026-01-30 15:28:52 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,8 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
-import Navbar from "@/components/navigation/navbar";
 import { Toaster } from "@/components/ui/toaster";
 import { auth } from "@/auth";
-
 import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({
@@ -13,6 +11,7 @@ const inter = Inter({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
 });
+
 const space_Grotesk = Space_Grotesk({
   variable: "--font-Space-Grotesk",
   subsets: ["latin"],
@@ -21,44 +20,42 @@ const space_Grotesk = Space_Grotesk({
 
 export const metadata: Metadata = {
   title: "Dev Overflow",
-  description:
-    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
-  icons: {
-    icon: "/images/site-logo.svg",
-  },
+  description: "A community-driven platform for asking and answering programming questions.",
+  icons: { icon: "/images/site-logo.svg" },
 };
 
 export default async function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   const session = await auth();
-  console.log("asuth", session)
+
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        
-            <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
-          
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
       </head>
-      <SessionProvider session={session}>
-        <body
-          className={`${inter.variable} ${space_Grotesk.variable} antialiased`}
-        >
+
+      <body
+        className={`${inter.variable} ${space_Grotesk.variable} antialiased`}
+      >
+        <SessionProvider session={session}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-            {/* <Navbar/> */}
-
             {children}
           </ThemeProvider>
-        </body>
-        <Toaster />
-      </SessionProvider>
+
+          <Toaster />
+        </SessionProvider>
+      </body>
     </html>
   );
 }
